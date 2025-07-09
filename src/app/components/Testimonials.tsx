f87218342cf41f42344b45674c6feec408760c90
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2025-07-09 17:16:54 -0300

    preenchedo dados com map

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,16 +1,28 @@
 import "../styles/Testimonials.css";
 function Testimonials() {
+  const testemoldial = [
+    {
+      nome: "– Aiko, Hacker Freelance",
+      relato: "Nunca experimentei nada tão imersivo. Parece que entrei em outra dimensão!"
+    },
+    {
+      nome: " Zero, Engenheiro de Códigos",
+      relato: "O futuro chegou. Implantes neurais e realidades paralelas em um só lugar"
+    }
+  ]
   return (
     <section className="testimonials-section">
       <h2>O que os usuários dizem</h2>
       <div className="testimonial">
-        <p>Nunca experimentei nada tão imersivo. Parece que entrei em outra dimensão!</p>
-        <span>– Aiko, Hacker Freelance</span>
+        {testemoldial.map((item, index) => (
+          <p key={index}>
+            <span>{item.relato}</span>
+            <strong>{item.nome}</strong>
+          </p>
+        ))}
+        
       </div>
-      <div className="testimonial">
-        <p>O futuro chegou. Implantes neurais e realidades paralelas em um só lugar.</p>
-        <span>– Zero, Engenheiro de Códigos</span>
-      </div>
+      
     </section>
   );
 }
